f3b52d004740bcb01c38f30cbec0cdea642bc5e6
harun <39270324+hbaklan943@users.noreply.github.com>
2024-11-28 02:38:50 +0300

    Update px4_simple_app.c

--- src/examples/px4_simple_app/px4_simple_app.c
+++ src/examples/px4_simple_app/px4_simple_app.c
@@ -1,130 +1,96 @@
-/****************************************************************************
- *
- *   Copyright (c) 2012-2019 PX4 Development Team. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name PX4 nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ****************************************************************************/
-
-/**
- * @file px4_simple_app.c
- * Minimal application example for PX4 autopilot
- *
- * @author Example User <mail@example.com>
- */
-
 #include <px4_platform_common/px4_config.h>
 #include <px4_platform_common/log.h>
 #include <px4_platform_common/tasks.h>
 #include <px4_platform_common/posix.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <poll.h>
-#include <string.h>
-#include <math.h>
-
+#include <drivers/drv_hrt.h>
 #include <uORB/uORB.h>
-#include <uORB/topics/vehicle_acceleration.h>
-#include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/target_locking_status.h>
+#include <pthread.h>
+#include <string.h>
 
-__EXPORT int px4_simple_app_main(int argc, char *argv[]);
+pthread_t publishing_thread;  // Thread handle for publishing
+bool is_running = false;      // Flag to control the publishing thread
 
-int px4_simple_app_main(int argc, char *argv[])
+// Thread function for publishing
+void* target_locking_publisher_thread(void* arg)
 {
-	PX4_INFO("Hello Sky!");
+    // Create topic structure
+    struct target_locking_status_s target_locking_status;
+    memset(&target_locking_status, 0, sizeof(target_locking_status));
 
-	/* subscribe to vehicle_acceleration topic */
-	int sensor_sub_fd = orb_subscribe(ORB_ID(vehicle_acceleration));
-	/* limit the update rate to 5 Hz */
-	orb_set_interval(sensor_sub_fd, 200);
+    // Advertise the topic
+    orb_advert_t target_locking_status_pub = orb_advertise(ORB_ID(target_locking_status), &target_locking_status);
 
-	/* advertise attitude topic */
-	struct vehicle_attitude_s att;
-	memset(&att, 0, sizeof(att));
-	orb_advert_t att_pub = orb_advertise(ORB_ID(vehicle_attitude), &att);
+    int count = 0;
+    while (true) {
+        if (!is_running) {  // Check if the thread should stop
+            PX4_INFO("Publishing thread stopping.");
+            break;  // Stop the thread if is_running is false
+        }
 
-	/* one could wait for multiple topics with this technique, just using one here */
-	px4_pollfd_struct_t fds[] = {
-		{ .fd = sensor_sub_fd,   .events = POLLIN },
-		/* there could be more file descriptors here, in the form like:
-		 * { .fd = other_sub_fd,   .events = POLLIN },
-		 */
-	};
+        // Update timestamp and alternate is_locked status
+        target_locking_status.timestamp = hrt_absolute_time();
+        target_locking_status.is_locked = (count % 2 == 0);
 
-	int error_counter = 0;
+        // Publish the topic
+        orb_publish(ORB_ID(target_locking_status), target_locking_status_pub, &target_locking_status);
 
-	for (int i = 0; i < 5; i++) {
-		/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
-		int poll_ret = px4_poll(fds, 1, 1000);
+        // Small delay (1000ms)
+        px4_usleep(1000000);
 
-		/* handle the poll result */
-		if (poll_ret == 0) {
-			/* this means none of our providers is giving us data */
-			PX4_ERR("Got no data within a second");
+        count++;
+    }
 
-		} else if (poll_ret < 0) {
-			/* this is seriously bad - should be an emergency */
-			if (error_counter < 10 || error_counter % 50 == 0) {
-				/* use a counter to prevent flooding (and slowing us down) */
-				PX4_ERR("ERROR return value from poll(): %d", poll_ret);
-			}
+    PX4_INFO("Publishing thread has stopped.");
+    return NULL;
+}
 
-			error_counter++;
+__EXPORT int px4_simple_app_main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        PX4_ERR("Usage: px4_simple_app <start|stop>");
+        return 1;
+    }
 
-		} else {
+    // Parse command line arguments
+    if (strcmp(argv[1], "start") == 0) {
+        if (is_running) {
+            PX4_INFO("Publishing thread is already running.");
+            return 0;
+        }
 
-			if (fds[0].revents & POLLIN) {
-				/* obtained data for the first file descriptor */
-				struct vehicle_acceleration_s accel;
-				/* copy sensors raw data into local buffer */
-				orb_copy(ORB_ID(vehicle_acceleration), sensor_sub_fd, &accel);
-				PX4_INFO("Accelerometer:\t%8.4f\t%8.4f\t%8.4f",
-					 (double)accel.xyz[0],
-					 (double)accel.xyz[1],
-					 (double)accel.xyz[2]);
+        is_running = true;  // Start the publishing thread
 
-				/* set att and publish this information for other apps
-				 the following does not have any meaning, it's just an example
-				*/
-				att.q[0] = accel.xyz[0];
-				att.q[1] = accel.xyz[1];
-				att.q[2] = accel.xyz[2];
+        // Initialize and create the publishing thread
+        int ret = pthread_create(&publishing_thread, NULL, target_locking_publisher_thread, NULL);
+        if (ret != 0) {
+            PX4_ERR("Failed to create publishing thread: %d", ret);
+            return 1;
+        }
 
-				orb_publish(ORB_ID(vehicle_attitude), att_pub, &att);
-			}
+        PX4_INFO("Publishing thread started.");
+    }
+    else if (strcmp(argv[1], "stop") == 0) {
+        if (!is_running) {
+            PX4_INFO("Publishing thread is not running.");
+            return 0;
+        }
 
-			/* there could be more file descriptors here, in the form like:
-			 * if (fds[1..n].revents & POLLIN) {}
-			 */
-		}
-	}
+        is_running = false;  // Stop the publishing thread
 
-	PX4_INFO("exiting");
+        // Wait for the publishing thread to finish
+        int ret = pthread_join(publishing_thread, NULL);
+        if (ret != 0) {
+            PX4_ERR("Failed to join publishing thread: %d", ret);
+            return 1;
+        }
 
-	return 0;
+        PX4_INFO("Publishing thread stopped.");
+    }
+    else {
+        PX4_ERR("Invalid argument. Usage: px4_simple_app <start|stop>");
+        return 1;
+    }
+
+    return 0;
 }
